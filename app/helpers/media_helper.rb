c6b819cf7b435f7f3148902ea9362ea975e36abb
shafouz <jlmacedomatos@gmail.com>
2021-03-16 19:19:29 -0400

    Form and some helpers

--- app/helpers/media_helper.rb
+++ app/helpers/media_helper.rb
@@ -6,4 +6,12 @@ module MediaHelper
   def rounded_pill
     "w-8 h-6 text-sm text-white bg-blue-500 rounded-full flex flex-none justify-center items-center font-semibold"
   end
+
+  def btn_primary
+    "py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-blue-500 hover:bg-blue-700 focus:outline-none"
+  end
+
+  def form_control
+    "border border-transparent rounded shadow leading-3 w-3/4 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+  end
 end
